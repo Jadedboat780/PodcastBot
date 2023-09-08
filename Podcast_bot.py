c76d369483c262b55b6709ca10f3a00f7cb132aa
Nikita Tokin <nikitatokin2006@gmail.com>
2023-09-08 23:01:51 +0300

    Переписал код бота с telebot на aiogram, написал свою библиотеку для работы с аудио

--- Podcast_bot.py
+++ Podcast_bot.py
@@ -1,105 +1,96 @@
-import telebot
-import yt_dlp
-import random
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, FSInputFile
+from aiogram.filters import CommandStart
+import aiofiles.os as aos
+
+from audio_lib import yt_dlp
+
 import re
-from separation_audio import split_audio, file_list
-from Podcast_bot_message import welcome_message, commands_dict, base_anecdote, url_error
+from typing import Optional
 
-bot = telebot.TeleBot('Ваш токен')
+from Podcast_bot_message import welcome_message, commands_dict
+from config import TOKEN
 
+import time
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    '''Приветствие нового пользователя'''
-    bot.send_message(message.chat.id, welcome_message)
+bot: Bot = Bot(token=TOKEN)
+dp: Dispatcher = Dispatcher()
 
-
-@bot.message_handler(commands=['author'])
-def author(message):
-    '''Команда, отправляющая пользователю ссылку на профиль создателя бота'''
-    bot.send_message(message.chat.id, commands_dict['author'])
+button_1: InlineKeyboardButton = InlineKeyboardButton(text='Начальное сообщение', callback_data='start')
+button_2: InlineKeyboardButton = InlineKeyboardButton(text='Помощь', callback_data='help')
+button_3: InlineKeyboardButton = InlineKeyboardButton(text='Директория проекта', callback_data='GitHub')
 
 
-@bot.message_handler(commands=['help'])
-def help(message):
-    '''Объясняет пользователю как работает бот'''
-    bot.send_message(message.chat.id, commands_dict['help'])
+@dp.message(CommandStart())
+async def start(message: Message):
+    keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=[[button_2], [button_3]])
+    await message.answer(text=welcome_message, reply_markup=keyboard)
 
 
-@bot.message_handler(commands=['GitHub_Directory'])
-def github(message):
-    '''Команда, отправляющая ссылку на директория бота на GitHub'''
-    bot.send_message(message.chat.id, commands_dict['GitHub_Directory'])
+@dp.callback_query(F.data == 'start')
+async def start_button(callback: CallbackQuery):
+    keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=[[button_2], [button_3]])
+    await callback.message.edit_text(text=welcome_message, reply_markup=keyboard)
 
 
-def Song(song_url, song_title):
-    '''Обработка видео из Ютуб в аудио файл'''
-    outtmpl = song_title
-    ydl_opts = {
-        'format': 'm4a/bestaudio/best',
-        'outtmpl': outtmpl,
-        'postprocessors': [
-            {'key': 'FFmpegExtractAudio',
-             'preferredcodec': 'm4a',
-             'preferredquality': '192'},
-            {'key': 'FFmpegMetadata'},
-        ],
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([song_url])  # Скачивание видео в виде m4a файла
+@dp.callback_query(F.data == 'help')
+async def help_button(callback: CallbackQuery):
+    keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_3]])
+    await callback.message.edit_text(text=commands_dict['help'], reply_markup=keyboard)
 
 
-def refactor_song_url(message):
-    '''Обработка ссылки в нужный формат'''
-    if '&' in message:
-        song_url = re.sub(r'&[\w\S]*', '', message)
-    elif 'youtu.be' in message:
-        song_url = re.sub('youtu.be/', 'www.youtube.com/watch?v=', message)
+@dp.callback_query(F.data == 'GitHub')
+async def github_button(callback: CallbackQuery):
+    keyboard: InlineKeyboardMarkup = InlineKeyboardMarkup(inline_keyboard=[[button_1], [button_2]])
+    await callback.message.edit_text(text=commands_dict['GitHub_Directory'], reply_markup=keyboard)
+
+
+def url_replace(url: str, /) -> str:
+    name = re.sub(".+\?[vsi]+=", "", url)
+    name = re.sub("&t=[0-9]+s", "", name)
+    return name
+
+
+@dp.message()
+async def url_link(message: Message):
+    url: str = message.text
+
+    if url == 'https://www.youtube.com' or url == 'https://www.youtube.com/':
+        await message.reply(text="Зачем вы отправляете ссылку на главную страницу ютуба?")
+
     else:
-        song_url = message
-    return song_url.split('watch?v=')[-1]
+        try:
+            start = time.perf_counter()
 
+            id: int = message.from_user.id
+            name: str = url_replace(url)
+            dirs: list[str] = await aos.listdir('audio')
 
-def send_audio(file_folder, song_url, id_chat):
-    for file in file_folder:
-        with open(f'content/{song_url}/{file}', 'rb') as audio:
-            bot.send_audio(id_chat, audio=audio)  # Отправка mp3 файла
+            if name not in dirs:
+                yt_dlp(url, name)
+                dirs: list[str] = await aos.listdir(f'audio/{name}')
+                for opus_file in dirs:
+                    send_file: FSInputFile = FSInputFile(path=f'audio/{name}/{opus_file}', filename=opus_file)
+                    await bot.send_voice(chat_id=id, voice=send_file, caption=send_file.filename)
 
-
-@bot.message_handler(content_types=['text'])
-def download(message):
-    '''#Функция принимает URL ссылку в виде текстового сообщения и обрабатывает её'''
-    try:
-        if 'https://www.youtube.com' == message.text or message.text == 'https://www.youtube.com/':
-            bot.send_message(message.chat.id, 'Зачем вы отправляете ссылку на главную страницу ютуба?')
-        elif ('www.youtube.com/watch?v=' in message.text) or ('youtu.be' in message.text):
-            number = random.randint(1, 24)
-            bot.reply_to(message, base_anecdote[number])  # Бот отправляет анекдот
-            song_url = refactor_song_url(message.text)
-
-            if song_url in file_list('content'):
-                file_folder = file_list(f'content/{song_url}')
-                send_audio(file_folder, song_url, message.chat.id)
             else:
-                Song(song_url, song_url)
-                file_folder = split_audio(f'{song_url}.m4a', song_url)
+                dirs: list[str] = await aos.listdir(f'audio/{name}')
+                for opus_file in dirs:
+                    send_file: FSInputFile = FSInputFile(path=f'audio/{name}/{opus_file}', filename=opus_file)
+                    await bot.send_voice(chat_id=id, voice=send_file, caption=send_file.filename)
 
-                if file_folder:
-                    send_audio(file_folder, song_url, message.chat.id)
-                else:
-                    bot.reply_to(message, 'Видео является слишком длинным')
-        else:
-            bot.send_message(message.chat.id, 'Неверные данные')
+        except ValueError:
+            await message.reply(text="Неправильный url")
 
-    except PermissionError:
-        file_list(None)
-        bot.send_message(message.chat.id, 'Хватит спамить бота')
+        except IOError as err:
+            await message.reply(text=err)
 
-    except yt_dlp.utils.DownloadError:
-        # Отправка одного из нескольких сообщений, в случае оправки неправильного url
-        number = random.randint(1, 5)
-        bot.send_message(message.chat.id, url_error[number])
+        except Exception as err:
+            print(f"URL: {url} \n NAME: {name} \n ERR: {err}")
+
+        finally:
+            print(time.perf_counter() - start)
 
 
 if __name__ == '__main__':
-    bot.polling()
+    dp.run_polling(bot, allowed_updates=dp.resolve_used_update_types())
