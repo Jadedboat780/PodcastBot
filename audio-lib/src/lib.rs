e3ab57a7a2b54e809edfae92daff2f90804828e5
nikit <nikitatokin2006@gmail.com>
2024-07-01 11:15:34 +0300

    Переписал реализацию библиотеки

--- audio-lib/src/lib.rs
+++ audio-lib/src/lib.rs
@@ -1,13 +1,11 @@
-mod async_lib;
-mod sync_lib;
+mod schemas;
+mod sync_mod;
+mod async_mod;
 
-use sync_lib::sync_mod;
-use async_lib::async_mod;
-use pyo3::prelude::{wrap_pyfunction, PyModule, PyResult, Python};
-use pyo3::pymodule;
+use pyo3::prelude::*;
 
 #[pymodule]
-fn audio_lib(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+fn audio_lib(py: Python, m: &PyModule) -> PyResult<()> {
     mod_sync(py, m)?;
     mod_async(py, m)?;
     Ok(())
@@ -15,18 +13,16 @@ fn audio_lib(py: Python<'_>, m: &PyModule) -> PyResult<()> {
 
 fn mod_sync(py: Python<'_>, audio_lib: &PyModule) -> PyResult<()> {
     let m = PyModule::new(py, "sync_mod")?;
-    m.add_function(wrap_pyfunction!(sync_mod::is_streaming, m)?)?;
+    m.add_function(wrap_pyfunction!(sync_mod::get_video_info, m)?)?;
     m.add_function(wrap_pyfunction!(sync_mod::download_audio, m)?)?;
-    m.add_function(wrap_pyfunction!(sync_mod::audio_separation, m)?)?;
     audio_lib.add_submodule(m)?;
     Ok(())
 }
 
 fn mod_async(py: Python<'_>, audio_lib: &PyModule) -> PyResult<()> {
     let m = PyModule::new(py, "async_mod")?;
-    m.add_function(wrap_pyfunction!(async_mod::is_streaming, m)?)?;
+    m.add_function(wrap_pyfunction!(async_mod::get_video_info, m)?)?;
     m.add_function(wrap_pyfunction!(async_mod::download_audio, m)?)?;
-    m.add_function(wrap_pyfunction!(async_mod::audio_separation, m)?)?;
     audio_lib.add_submodule(m)?;
     Ok(())
 }
