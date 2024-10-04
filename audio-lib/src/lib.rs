b0860da208f874b63224031d95c42f80f58bb558
tokin_nikita <nikitatokin2006@gmail.com>
2024-10-04 18:55:18 +0300

    updated the module system

--- audio-lib/src/lib.rs
+++ audio-lib/src/lib.rs
@@ -1,26 +1,24 @@
-mod schemas;
-mod sync_mod;
 mod async_mod;
+mod models;
+mod sync_mod;
 
+use models::VideoInfo;
 use pyo3::prelude::*;
 
 #[pymodule]
 fn audio_lib(py: Python, m: &PyModule) -> PyResult<()> {
-    mod_sync(py, m)?;
-    mod_async(py, m)?;
-    Ok(())
-}
+    m.add_class::<VideoInfo>()?;
 
-fn mod_sync(py: Python<'_>, audio_lib: &PyModule) -> PyResult<()> {
-    let m = PyModule::new(py, "sync_mod")?;
     m.add_function(wrap_pyfunction!(sync_mod::get_video_info, m)?)?;
     m.add_function(wrap_pyfunction!(sync_mod::download_audio, m)?)?;
-    audio_lib.add_submodule(m)?;
+
+    async_module(py, m)?;
+
     Ok(())
 }
 
-fn mod_async(py: Python<'_>, audio_lib: &PyModule) -> PyResult<()> {
-    let m = PyModule::new(py, "async_mod")?;
+fn async_module(py: Python<'_>, audio_lib: &PyModule) -> PyResult<()> {
+    let m = PyModule::new(py, "asyncio")?;
     m.add_function(wrap_pyfunction!(async_mod::get_video_info, m)?)?;
     m.add_function(wrap_pyfunction!(async_mod::download_audio, m)?)?;
     audio_lib.add_submodule(m)?;
